7462800f5dc1772b99e241c7adb1c2c876677e64
Luis Rodriguez <luisu_10@hotmail.com>
2022-06-21 17:57:41 -0500

    finish

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use("/assets", express.static("assets"));
 app.use("/dist", express.static("dist"));
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
